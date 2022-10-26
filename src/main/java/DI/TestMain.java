04623f1ed69891ca4d7b961d42dc0b5e88a99f03
wogy2829@naver.com <wogy2829@naver.com>
2022-10-26 10:03:42 +0900

    springboot mvc 기초

--- src/main/java/DI/TestMain.java
+++ src/main/java/DI/TestMain.java
@@ -3,7 +3,7 @@ package DI;
 public class TestMain {
 
     public static void main(String[] args) {
-        Test test = new Test(new DITestImpl2());
+        Test test = new Test(new DITestImpl());
 
         test.show();
     }
